0d72a882ff9873b47faa470eff1d2063b99e8c14
Arun Sharma <arunnssharma06@gamil.com>
2019-11-21 12:15:33 +0545

    Initial commit

---
+++ app/src/main/java/arun_sharma/first/resortmanagementsystem/MainActivity.java
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="utf-8"?>
+<LinearLayout
+    xmlns:android="http://schemas.android.com/apk/res/android"
+            xmlns:app="http://schemas.android.com/apk/res-auto"
+            xmlns:tools="http://schemas.android.com/tools"
+            android:layout_width="match_parent"
+            android:layout_height="match_parent"
+            android:orientation="vertical"
+            android:gravity="centre"
+            tools:context=".MainActivity">
+
+<ImageView
+      android:layout_width="150dp"
+              android:layout_height="150dp"
+              android:src="@drawable/data"/>
+<Linearlayout
+    android:layout_width="wrap_content"
+            android:layout_height="wrap_content"
+            android:padding="10dp"
+            android:gravity="centre"
+            android:layout_margintop="30dp"
+            android:background="@drawable/round_edittext"></Linearlayout>
+
+</LinearLayout>
